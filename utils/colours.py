4d007c2c0219a4bf9621a2b074e2beb027630922
rajrocksdeworld <rajrocksdeworld@gmail.com>
2019-07-22 23:46:09 +0530

    Colors Added

---
+++ utils/colours.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 22 23:35:51 2019
+
+@author: Rajkumar
+"""
+
+gray=(119,118,110)
+black=(0,0,0)
+red=(255,0,0)
+green=(0,200,0)
+blue=(0,0,200)
+bright_red=(255,0,0)
+bright_green=(0,255,0)
+bright_blue=(0,0,255)
